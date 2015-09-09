be9b84b18a384d904fc2fd0f174b184f0db92dc6
Ru-T <rwajnberg@gmail.com>
2015-09-09 16:45:01 -0400

    Generate random fires from computer.

--- computer_player.rb
+++ computer_player.rb
@@ -8,4 +8,22 @@ class ComputerPlayer < Player
     @ships = []
   end
 
+  def place_ships(ship_sizes)
+    ship_sizes.each do |size|
+      @ships << Ship.new(size)
+    end
+    #@ships.each do |ship|
+      # position = get_user_input
+      # across = get_user_input
+      # across.downcase!
+      # across == "across" ? (across = true) : (across = false)
+      # @grid.place_ship(ship, @grid.x_of(position), @grid.y_of(position), across)
+  #  end
+      puts "HAL 9000 has placed its ships."
+  end
+
+  def call_shot
+    "#{("A".."J").to_a.sample}" + "#{rand(1..10)}"
+  end
+
 end
