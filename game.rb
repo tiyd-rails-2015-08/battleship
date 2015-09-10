014264876fdddbf2df1135da4b99bbeca9e14625
David Bernheisel <david@bernheisel.com>
2015-09-09 22:46:46 -0400

    Test passed up to 42

--- game.rb
+++ game.rb
@@ -1,35 +1,76 @@
 require './human_player'
 require './computer_player'
-require './grid'
-require './ship'
 
 class Game
-  attr_accessor :shiplengths
+  attr_accessor :ship_lengths
   attr_accessor :salvo
   attr_accessor :player_one
   attr_accessor :player_two
+  attr_accessor :player_one_turn
 
-  def initialize(player_one, player_two, shiplengths = [5,4,3,3,2], salvo = false)
+  def initialize(player_one, player_two, ship_lengths = [2,3,3,4,5], salvo = false)
     @player_one = player_one
     @player_two = player_two
-    @shiplengths = shiplengths
+    @ship_lengths = ship_lengths
     @salvo = salvo
+    @player_one_turn = true #[false,true].sample
   end
 
   def welcome
-    puts "Welcome, #{@player_one.name} and #{@player_two.name}!\nIt's time to play Battleship.\n"
+    puts "Welcome, #{@player_one.name} and #{@player_two.name}!\nIt's time to play Battleship."
   end
 
   def display_status
-    #TODO
+    @player_one_turn ? player = @player_one : player = @player_two
+    shots_board = Hash.new(" ")
+    player.shots_fired.each do |shot|
+      next unless shot.position || (shot.x && shot.y)
+      shot.hit == true ? shots_board[shot.position.to_sym] = "+" : shots_board[shot.position.to_sym] = "-"
+    end
+
+    puts "SHOTS TAKEN:"
+    player.grid.display(shots_board)
+    puts ""
+    puts "YOUR BOARD:"
+    player.grid.display
   end
 
   def take_turn
-    #TODO
+    shooter, victim, guess = nil
+    if @player_one_turn
+      shooter = @player_one
+      victim = @player_two
+      @player_one_turn = false
+    else
+      shooter = @player_two
+      victim = @player_one
+      @player_one_turn = true
+    end
+
+
+    guess = shooter.call_shot
+    x = victim.grid.x_of(guess)
+    y = victim.grid.y_of(guess)
+    if victim.grid.fire_at(x, y)
+      puts("Hit!")
+      shooter.shots_fired.last.hit = true
+    else
+      puts("Miss!")
+    end
   end
 
   def play
-    #TODO
+    self.welcome
+    loop do
+      break if @player_one.grid.sunk? || @player_two.grid.sunk?
+      self.take_turn
+    end
+    @player_one.grid.sunk? ? puts("Congratulations, #{@player_two.name}!") : puts("Congratulations, #{@player_one.name}!")
+  end
+
+  def place_ships
+    @player_one.place_ships(@ship_lengths)
+    @player_two.place_ships(@ship_lengths)
   end
 
 end
---
+++ position.rb
@@ -0,0 +1,6 @@
+class Position
+  attr_accessor :status
+  attr_accessor :x
+  attr_accessor :y
+  attr_accessor :position
+end
