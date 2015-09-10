8757efdb42629d1a0d4d07b37da6f78adf3915b7
Julie David <julie.angela.david@gmail.com>
2015-09-10 16:24:54 -0400

    Game requires players.

--- game.rb
+++ game.rb
@@ -1,7 +1,14 @@
 
 class Game
+  attr_reader :human, :computer
+  def initialize(human, computer)
+    puts ("Welcome, Frank and HAL 9000!\nIt's time to play Battleship.\n") do
+      game.welcome
+    end
+  end
 
+  def welcome
 
-
-
+  end
+  
 end
