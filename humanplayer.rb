244c475b4bb08b3f94b3d8ff743526795a65d3ab
Julie David <julie.angela.david@gmail.com>
2015-09-10 13:55:00 -0400

    Test 30 almost passes.

--- humanplayer.rb
+++ humanplayer.rb
@@ -11,23 +11,20 @@ class HumanPlayer < Player
   end
 
   def place_ships(length_array = [2, 3, 3, 4, 5])
-    # counter = 0
-    # length_array.length.times do
-    #   puts "#{name}, where would you like to place a ship of length #{length_array[counter]}?\nAcross or Down?\n"
-    #   input = get_user_input
-    #   if input.length == 2
-    #     x = grid.x_of(input)
-    #     y = grid.y_of(input)
-    #   elsif input == "Across"
-    #     horizontal = true
-    #   elsif input == "Down"
-    #     horizontal = false
-    #   else
-    #     puts "Error"
-    #   end
-    #   @ships << grid.place_ship(Ship.new(length_array[counter]), x, y, horizontal)
-    #   counter +=
-    # end
+    length_array.each do |s|
+      puts "#{name}, where would you like to place a ship of length #{s}?"
+      input = get_user_input
+        x = grid.x_of(input)
+        y = grid.y_of(input)
+      puts "Across or Down?"
+      if input == "Across"
+        horizontal = true
+      elsif input == "Down"
+        horizontal = false
+      else
+      end
+      @ships << grid.place_ship(Ship.new(s), x, y, horizontal)
+    end
   end
 
 end
