5b3e8a283700b7b023306070c3fc7e5984abffad
Doug Bryant <douglas.e.bryant@gmail.com>
2015-09-11 00:24:27 -0400

    Forgot to remove merge conflicts

--- ship.rb
+++ ship.rb
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 require './grid'
 
-=======
->>>>>>> 0ecbbb936b6773e8051a3693c5f60bb9189cef59
 class Ship
   attr_reader :length
   def initialize(length)
@@ -32,24 +30,15 @@ class Ship
   def overlaps_with?(other_ship)
     @positions.any? {|p| other_ship.covers?(p[0],p[1])}
 
-<<<<<<< HEAD
     # @positions.each do |place|
     #   return true if other_ship.covers?(place[0], place[1])
-=======
-    # @positions.each |place|
-    #   return true if other_ship.covers?(place[0],place[1])
->>>>>>> 0ecbbb936b6773e8051a3693c5f60bb9189cef59
     # end
     # false
   end
 
   def fire_at(x, y)
     if covers?(x, y) && !@hits.include?([x, y])
-<<<<<<< HEAD
-    @hits << [x, y]
-=======
       @hits << [x, y]
->>>>>>> 0ecbbb936b6773e8051a3693c5f60bb9189cef59
     end
   end
 
@@ -60,8 +49,5 @@ class Ship
   def hit_on?(x, y)
     @hits.include?([x, y])
   end
-<<<<<<< HEAD
 
-=======
->>>>>>> 0ecbbb936b6773e8051a3693c5f60bb9189cef59
 end
