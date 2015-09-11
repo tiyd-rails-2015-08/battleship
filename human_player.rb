13cd99a0f619e014303dd91777bee7ca5db2f013
Ashley Mason <amason@spoonflower.com>
2015-09-11 07:27:52 -0400

    working on test #30 though it is not passing yet.

--- human_player.rb
+++ human_player.rb
@@ -1,8 +1,20 @@
 require './player.rb'
 
+def get_user_input
+  gets.chomp
+end
+
 class HumanPlayer < Player
   def initialize(name = "Dave")
     @name = name
   end
 
 end
+
+puts "#{@name}, where would you like to place a ship of length 2?"
+
+puts "Across or Down?"
+
+puts "#{@name}, where would you like to place a ship of length 5?"
+
+puts "Across or Down?"
